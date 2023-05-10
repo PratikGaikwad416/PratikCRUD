570ea424a6c46f17d9c150e72d0c00dac829131c
PratikGaikwad416 <129584349+PratikGaikwad416@users.noreply.github.com>
2023-05-11 02:22:17 +0530

    Add files via upload

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using CrudAssignment.Dal;
+using CrudAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudAssignment.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public ActionResult Index()
+        {
+            CategoryDal dal = new CategoryDal();
+            List<Categories> list = dal.CategoriesList();
+            return View(list);
+        }
+
+
+        [HttpPost]
+        public JsonResult Insert(Categories cat)
+        {
+            CategoryDal dal = new CategoryDal();
+            int status = dal.insert(cat);
+            return Json(status, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        [HttpPost]
+        public JsonResult Update(Categories cat)
+        {
+            CategoryDal dal = new CategoryDal();
+            int status = dal.update(cat);
+            return Json(status, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult Delete(Categories cat)
+        {
+            CategoryDal dal = new CategoryDal();
+            int status = dal.delete(cat.id);
+            return Json(status, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
