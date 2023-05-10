bda13c27b31ccfd4cee1501c52046be7e3628539
PratikGaikwad416 <129584349+PratikGaikwad416@users.noreply.github.com>
2023-05-11 02:24:43 +0530

    Add files via upload

---
+++ Models/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudAssignment.Models
+{
+    public class Categories
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudAssignment.Models
+{
+    public class Products
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public int categoryid { get; set; }
+
+        public string categoryname { get; set; }
+    }
+}
